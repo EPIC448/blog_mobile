774c19d060b7feef028a6242a82366e4cd9e09f2
EPIC448 <samrey2018@gmail.com>
2021-08-16 18:01:16 -0400

    added a IndexScreen and application is working

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import {  createAppContainer } from 'react-navigation';
+import IndexScreen from './src/screens/IndexScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Application is working an it Life!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen
   },
-});
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs'
+    }
+  }
+);
+
+const App =  createAppContainer(navigator)
+
+export default () => {
+  return <App />;
+} 
