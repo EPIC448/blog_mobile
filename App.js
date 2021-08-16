f546c99601d482e0d003a79a485f11e2a62ae0a0
EPIC448 <samrey2018@gmail.com>
2021-08-16 17:21:05 -0400

    Packages installed for React native: gesture-handler reanimted screens safe-area-contet community/masked-view stack

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Application is working an it Life!</Text>
       <StatusBar style="auto" />
     </View>
   );
