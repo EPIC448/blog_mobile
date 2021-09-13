7038fea0a24186a9a7b2ec7e0876b15bf34e82a9
EPIC448 <samrey2018@gmail.com>
2021-09-13 17:42:52 -0400

    updating reducer with the Delete function that works

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,6 +3,8 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'delete_blogpost':
+            return state.filter((blogPost) => blogPost.id !== action.payload);
         case 'add_blogpost':
             return [...state, {
                 id: Math.floor(Math.random() * 99999),
@@ -21,8 +23,14 @@ const addBlogPost = (dispatch) => {
     return () => {
         dispatch({ type: 'add_blogpost' })
     };
+};
+    
+const deleteBlogPost = dispatch => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id })
     };
+}
 
-export const {Context, Provider }= createDataContext(blogReducer,{ addBlogPost},[])
+export const {Context, Provider }= createDataContext(blogReducer,{ addBlogPost, deleteBlogPost},[])
  
 
