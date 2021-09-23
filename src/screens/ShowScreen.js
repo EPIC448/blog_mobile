78c713bb61e6c7104875f6cbac1d0ac328d3aa36
EPIC448 <samrey2018@gmail.com>
2021-09-23 17:41:26 -0400

    Edit the Icon link

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,8 @@
 import React, { useContext} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Context } from '../context/BlogContext';
+import { EvilIcons } from '@expo/vector-icons';
 
 
 const ShowScreen = ({ navigation }) => {
@@ -11,8 +13,19 @@ const ShowScreen = ({ navigation }) => {
     return (
         <View>
             <Text>{blogPost.title}</Text>
+            <Text>{blogPost.content}</Text>
         </View>
     );
+};
+
+ShowScreen.navigationOptions = ({navigation}) => {
+    return {
+        headerRight: () => {
+            <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+                <EvilIcons name = "pencil" size={35} />
+            </TouchableOpacity>
+  }
+    };
 };
  
 const styles = StyleSheet.create({});
